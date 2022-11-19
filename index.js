3c1334993485bd5b96c2e184e27f5fb436e02d67
Jose Armas <armasjose95@gmail.com>
2022-11-19 16:22:29 -0600

    added all blocks

--- index.js
+++ index.js
@@ -15,16 +15,43 @@ class block{
     }
 }
 
-//draw my block
+//all my blocks
+const blocks = [
+    new block(10,270), // x and y axis
+    new block(120,270),
+    new block(230,270),
+    new block(340,270),
+    new block(450,270),
+    new block(10,240), 
+    new block(120,240),
+    new block(230,240),
+    new block(340,240),
+    new block(450,240),
+    new block(10,210),
+    new block(120,210),
+    new block(230,210),
+    new block(340,210),
+    new block(450,210),
+]
+
+
+
+
+//draw all my blocks
 //only when we cann this function, we will create a block
-function addBlock() {
+function addBlocks() {
+    for (let i = 0; i < blocks.length; i++) { //as long as i is smaller than blocks array lenght, add 1
     const block = document.createElement('div') //created a div using a js method and stored it as a block
     block.classList.add('block') //Give block the classList with the style of block located in css
-    block.style.left = '100px' //left & bottom corner's margin are the same from the anchor point(bttm and left corner)
-    block.style.bottom = '50px'
+    //left & bottom corner's margin are the same from the anchor point(bttm and left corner)
+    // going into blocks array next line down. Assigning x axis to the left. give styling of 10 px(x axis)
+    block.style.left = blocks[i].bottomLeft[0] + 'px'
+    //getting 2nd value(y axis which is 270px)
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px'
     grid.appendChild(block) //now that we have grid we put in newly created block using appchild with the style of block located in css
+    }
 }
 
-addBlock()
+addBlocks()
 
-//
+
